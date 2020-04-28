aecd0fca911d8c06c7277945c129e8444045eccb
sam7kr <sam7kr@gmail.com>
2020-04-28 18:04:09 +0100

    now every thing is runing

--- src/preload.js
+++ src/preload.js
@@ -25,7 +25,36 @@ window.addEventListener('DOMContentLoaded', () => {
     	document.getElementById("divOne").innerText = message
     	})
 
+    function buttonOneOn(){
+        console.log(`sending \'2on\' over tcp`)
         pyshell.send('2on')
+    }
+
+    function buttonOneOff(){
+        console.log(`sending \'2off\' over tcp`)
+        pyshell.send('2off')
+    }
+
+    function buttonTwoOn(){
+        console.log(`sending \'3on\' over tcp`)
+        pyshell.send('3on')
+    }
+
+    function buttonTwoOff(){
+        console.log(`sending \'3off\' over tcp`)
+        pyshell.send('3off')
+    }
+
+    function buttonKill(){
+        console.log(`sending \'kill\' over tcp`)
+        pyshell.send('kill')
+    }
+      
+    document.getElementById("buttonOneOn").addEventListener("click", buttonOneOn)
+    document.getElementById("buttonOneOff").addEventListener("click", buttonOneOff)
+    document.getElementById("buttonTwoOn").addEventListener("click", buttonTwoOn)
+    document.getElementById("buttonTwoOff").addEventListener("click", buttonTwoOff)
+    document.getElementById("kill").addEventListener("click", buttonKill)
 
 })
 
